edadcb03969ffdc31a9d6c0349cdc60c4c281631
Amandeep Ghuman <aman.ghuman30@gmail.com>
2021-05-14 18:25:43 -0700

    location permission request and handle

--- app/src/main/java/com/runtracker/app/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/runtracker/app/ui/fragments/RunFragment.kt
@@ -1,27 +1,77 @@
 package com.runtracker.app.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.runtracker.app.R
+import com.runtracker.app.util.Constants
+import com.runtracker.app.util.TrackingUtility
 import com.runtracker.app.viewModels.MainViewModel
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run) ,EasyPermissions.PermissionCallbacks{
 
     private val mainViewModel : MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermissions()
         addRunFab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
 
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermission(requireContext()))
+            return
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.request_permission_text),
+                Constants.PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.request_permission_text),
+                Constants.PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireContext()).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
 }
--- app/src/main/java/com/runtracker/app/util/Constants.kt
+++ app/src/main/java/com/runtracker/app/util/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
 
     companion object {
         const val RUNNING_DATABASE_NAME = "running_database.db"
+
+        const val PERMISSION_REQUEST_CODE = 10
     }
 }
